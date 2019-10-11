b074fcdd6615fb827f668a3486b74155168c0198
Paulo <pdichone@gmail.com>
2019-10-11 15:57:57 -0700

    Update CreatePost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,67 +1,90 @@
-import  React, { Component } from "react";
-import { Mutation } from "react-apollo";
+import React, { Component } from "react";
+import { API, graphqlOperation } from 'aws-amplify'
 import { createPost } from '../graphql/mutations';
-import gql from "graphql-tag";
+import { Auth } from 'aws-amplify';
 
 
 
 
+class CreatePost extends Component {
 
-class CreatePost extends React.Component {
-    handleSubmit = (e, createPost) => {
-      e.preventDefault();
-      createPost({
-        variables: {
-          input: {
-            title: this.title.value,
-            body: this.body.value,
-            createdAt: new Date().toISOString()
-          }
-        }
-      }).then(res => {
-        this.title.value = "";
-        this.body.value = "";
-      });
-    };
-    render() {
-      return (
-        <div>
-      
-          <Mutation mutation={gql(createPost)}>
-            {(createPost, { data, loading, error }) => {
-              return (
-                <div>
-                  <form
-                    className="add-post"
-                    onSubmit={e => this.handleSubmit(e, createPost)}
-                  >
-                    <input
-                     style={{fontSize: '19px'}}
-                      type="text" placeholder="Title"
-                      
-                      ref={node => (this.title = node)}
-                      required
-                    />
-                    <input
-                     style={{height:'150px', fontSize: '19px'}}
-                      type="text"
-                      placeholder="Body"
-                      ref={node => (this.body = node)}
-                      required
-                    />
-                    <input type="submit" value={loading ? "Hooray!": "Save Blog Post"}
-                   className="btn"
-                    style={{fontSize: '19px'}}
-                   />
-                  </form>
-                  {error && <p>{error.message}</p>}
-                </div>
-              );
-            }}
-          </Mutation>
-        </div>
-      );
-    }
+  state = {
+
+    postOwnerId: '',
+    postOwnerUsername: '',
+    numberLikes: 0,
+    postTitle: "",
+    postBody: "",
   }
-  
-  export default CreatePost;
+
+  componentDidMount = async () => {
+
+    await Auth.currentUserInfo()
+      .then(user => {
+        this.setState(
+          {
+            postOwnerUsername: user.username,
+            postOwnerId: user.attributes.sub,
+          }
+        )
+      })
+  }
+
+  handleChangePost = event => this.setState({ [event.target.name]: event.target.value })
+
+  handleAddPost = async event => {
+
+    event.preventDefault()
+
+    const input = {
+      postOwnerId: this.state.postOwnerId, // Todo: will add id automatically later
+      postOwnerUsername: this.state.postOwnerUsername,
+      postTitle: this.state.postTitle,
+      postBody: this.state.postBody,
+      createdAt: new Date().toISOString(),
+    }
+
+
+    await API.graphql(graphqlOperation(createPost, { input }))
+
+    this.setState({ postTitle: "", postBody: "" })// clear fields
+  }
+
+  render() {
+    return (
+      <div>
+
+        <form className="add-post"
+          onSubmit={this.handleAddPost}>
+
+          <input
+            style={{ fontSize: '19px' }}
+            type="text" placeholder="Title"
+            name="postTitle"
+            required
+            value={this.state.postTitle}
+            onChange={this.handleChangePost}
+
+          />
+
+          <textarea
+            type="text"
+            name="postBody"
+            rows="3"
+            cols="40"
+            required
+            placeholder="Post your thought"
+            value={this.state.postBody}
+            onChange={this.handleChangePost}
+          />
+          <input type="submit"
+            className="btn"
+            style={{ fontSize: '19px' }} />
+
+        </form>
+
+      </div>
+    )
+  }
+}
+export default CreatePost;
