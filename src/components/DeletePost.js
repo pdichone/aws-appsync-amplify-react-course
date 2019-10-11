779e8483c6dda606cd53add056cf6da234d78762
Paulo <pdichone@gmail.com>
2019-10-11 15:58:59 -0700

    Update DeletePOst

--- src/components/DeletePost.js
+++ src/components/DeletePost.js
@@ -1,58 +1,30 @@
-import React, {Component} from 'react'
-import { Mutation } from 'react-apollo'
+import React, { Component } from 'react'
 import { deletePost } from '../graphql/mutations'
-import gql from 'graphql-tag'
-import { listPosts } from '../graphql/queries'
+
+import { API, graphqlOperation } from 'aws-amplify';
 
 
 class DeletePost extends Component {
-    
-    handleDelete = (deletePost) => {
-         deletePost({
-              variables: {
-                   input: {
-                       id: this.props.id
-                   }
-              },
-              //optimistic response
-               optimisticResponse: () => ({
-                   deletePost: {
-                       //Must match the return type of the query below (listPosts)
-                       __typename: 'ModelPostConnection',
-                       id: this.props.id,
-                       title: this.props.title,
-                       body: this.props.body,
-                       createdAt: this.props.createdAt
-                   }
-               }),
-               update: (cache, { data: {deletePost}}) => {
-                  const query = gql(listPosts)
 
-                  //Read query from cache
-                  const data = cache.readQuery({ query })
+    handleDeletePost = async postId => {
 
-                  //Add updated postList to the cache copy
-                  data.listPosts.items = [
-                       ...data.listPosts.items.filter(item =>
-                          item.id !== this.props.id)
-                  ]
+        const input = {
+            id: postId
+        }
 
-                  //Overwrite the cache with the new results
-                  cache.writeQuery({ query, data })
-               }
-         })
+        await API.graphql(graphqlOperation(deletePost, { input }))
+
     }
 
+
     render() {
-         return (
-            <Mutation mutation={gql(deletePost)}>
-            {(deletePost, { loading, error }) => {
-                return <button onClick={
-                   () => this.handleDelete(deletePost)}>
-                    Delete Blog Post</button>
-            }}
-        </Mutation>
-         )
+        const post = this.props.data
+        //const post = this.state.posts
+        return (
+            <button onClick={() => this.handleDeletePost(post.id)} >
+                Delete Post
+            </button>
+        )
     }
 }
 export default DeletePost
