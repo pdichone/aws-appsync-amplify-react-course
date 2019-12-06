4bf7afb6697a1cc8c0312fe9c5537c09bba1478f
Paulo <pdichone@gmail.com>
2019-12-06 12:19:08 -0800

    Update DeletePost.js

--- src/components/DeletePost.js
+++ src/components/DeletePost.js
@@ -1,30 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component} from 'react'
+import { API, graphqlOperation } from 'aws-amplify'
 import { deletePost } from '../graphql/mutations'
-
-import { API, graphqlOperation } from 'aws-amplify';
-
 
 class DeletePost extends Component {
 
+
     handleDeletePost = async postId => {
+         const input = {
+              id: postId
+         }
 
-        const input = {
-            id: postId
-        }
-
-        await API.graphql(graphqlOperation(deletePost, { input }))
-
+         await API.graphql(graphqlOperation(deletePost, {input}))
     }
-
-
     render() {
         const post = this.props.data
-        //const post = this.state.posts
-        return (
-            <button onClick={() => this.handleDeletePost(post.id)} >
-                Delete Post
-            </button>
-        )
+         return (
+             <button onClick= { () => this.handleDeletePost(post.id) }>Delete</button>
+         )
     }
 }
-export default DeletePost
+export default DeletePost;
