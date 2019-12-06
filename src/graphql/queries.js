16c1f9d4bc6bb3f1e0a3bedffdda14f902ca009d
Paulo <pdichone@gmail.com>
2019-12-06 12:12:40 -0800

    Update queries.js

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,37 +1,164 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPost = `query GetPost($id: ID!) {
+  getPost(id: $id) {
+    id
+    postOwnerId
+    postOwnerUsername
+    postTitle
+    postBody
+    createdAt
+    comments {
+      items {
+        id
+        commentOwnerId
+        commentOwnerUsername
+        content
+        createdAt
+      }
+      nextToken
+    }
+    likes {
+      items {
+        id
+        numberLikes
+        likeOwnerId
+        likeOwnerUsername
+      }
+      nextToken
+    }
+  }
+}
+`;
 export const listPosts = `query ListPosts(
-$filter: ModelPostFilterInput
-$limit: Int
-$nextToken: String
+  $filter: ModelPostFilterInput
+  $limit: Int
+  $nextToken: String
 ) {
-listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
-items {
-id
-postOwnerId
-postOwnerUsername
-postTitle
-postBody
-createdAt
-comments {
-items {
-id
-commentOwnerId
-commentOwnerUsername
-content
-createdAt
+  listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        items {
+           id
+           commentOwnerId
+           commentOwnerUsername
+           content
+           createdAt
+        }
+      }
+      likes {
+        items {
+          id
+          numberLikes
+          likeOwnerId
+          likeOwnerUsername
+        }
+      }
+    }
+    nextToken
+  }
 }
-nextToken
+`;
+export const getComment = `query GetComment($id: ID!) {
+  getComment(id: $id) {
+    id
+    commentOwnerId
+    commentOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+    content
+    createdAt
+  }
 }
-likes {
-items {
-id
-numberLikes
-likeOwnerId
-likeOwnerUsername
+`;
+export const listComments = `query ListComments(
+  $filter: ModelCommentFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listComments(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      commentOwnerId
+      commentOwnerUsername
+      post {
+        id
+        postOwnerId
+        postOwnerUsername
+        postTitle
+        postBody
+        createdAt
+      }
+      content
+      createdAt
+    }
+    nextToken
+  }
 }
-nextToken
+`;
+export const getLike = `query GetLike($id: ID!) {
+  getLike(id: $id) {
+    id
+    numberLikes
+    likeOwnerId
+    likeOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+  }
 }
-}
-nextToken
-}
+`;
+export const listLikes = `query ListLikes(
+  $filter: ModelLikeFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listLikes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      numberLikes
+      likeOwnerId
+      likeOwnerUsername
+      post {
+        id
+        postOwnerId
+        postOwnerUsername
+        postTitle
+        postBody
+        createdAt
+      }
+    }
+    nextToken
+  }
 }
 `;
