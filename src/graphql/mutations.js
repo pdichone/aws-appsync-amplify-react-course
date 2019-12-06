5a8fff6c15a3029cc0a49f8d57bb0bd72c40ff7b
Paulo <pdichone@gmail.com>
2019-12-06 12:12:01 -0800

    Update mutations.js
    Added the correct mutations.

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -4,27 +4,231 @@
 export const createPost = `mutation CreatePost($input: CreatePostInput!) {
   createPost(input: $input) {
     id
-    title
-    body
+    postOwnerId
+    postOwnerUsername
+    postTitle
+    postBody
     createdAt
+    comments {
+      items {
+        id
+        commentOwnerId
+        commentOwnerUsername
+        content
+        createdAt
+      }
+      nextToken
+    }
+    likes {
+      items {
+        id
+        numberLikes
+        likeOwnerId
+        likeOwnerUsername
+      }
+      nextToken
+    }
   }
 }
 `;
 export const updatePost = `mutation UpdatePost($input: UpdatePostInput!) {
   updatePost(input: $input) {
     id
-    title
-    body
+    postOwnerId
+    postOwnerUsername
+    postTitle
+    postBody
     createdAt
+    comments {
+      items {
+        id
+        commentOwnerId
+        commentOwnerUsername
+        content
+        createdAt
+      }
+      nextToken
+    }
+    likes {
+      items {
+        id
+        numberLikes
+        likeOwnerId
+        likeOwnerUsername
+      }
+      nextToken
+    }
   }
 }
 `;
 export const deletePost = `mutation DeletePost($input: DeletePostInput!) {
   deletePost(input: $input) {
     id
-    title
-    body
+    postOwnerId
+    postOwnerUsername
+    postTitle
+    postBody
     createdAt
+    comments {
+      items {
+        id
+        commentOwnerId
+        commentOwnerUsername
+        content
+        createdAt
+      }
+      nextToken
+    }
+    likes {
+      items {
+        id
+        numberLikes
+        likeOwnerId
+        likeOwnerUsername
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const createComment = `mutation CreateComment($input: CreateCommentInput!) {
+  createComment(input: $input) {
+    id
+    commentOwnerId
+    commentOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+    content
+    createdAt
+  }
+}
+`;
+export const updateComment = `mutation UpdateComment($input: UpdateCommentInput!) {
+  updateComment(input: $input) {
+    id
+    commentOwnerId
+    commentOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+    content
+    createdAt
+  }
+}
+`;
+export const deleteComment = `mutation DeleteComment($input: DeleteCommentInput!) {
+  deleteComment(input: $input) {
+    id
+    commentOwnerId
+    commentOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+    content
+    createdAt
+  }
+}
+`;
+export const createLike = `mutation CreateLike($input: CreateLikeInput!) {
+  createLike(input: $input) {
+    id
+    numberLikes
+    likeOwnerId
+    likeOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const updateLike = `mutation UpdateLike($input: UpdateLikeInput!) {
+  updateLike(input: $input) {
+    id
+    numberLikes
+    likeOwnerId
+    likeOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const deleteLike = `mutation DeleteLike($input: DeleteLikeInput!) {
+  deleteLike(input: $input) {
+    id
+    numberLikes
+    likeOwnerId
+    likeOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
   }
 }
 `;
