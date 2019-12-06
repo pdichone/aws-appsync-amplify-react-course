363c89069c71fa84e3d4bea3fcfe19c1cb28062a
Paulo <pdichone@gmail.com>
2019-12-06 12:08:00 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import CreatePost from './components/CreatePost';
 import DisplayPosts from './components/DisplayPosts'
-
-//====== Start Auth config =====
-//auth with appsync
+import CreatePost from './components/CreatePost';
 import { withAuthenticator } from 'aws-amplify-react'
-import Amplify from 'aws-amplify'
 
-//Get the resources configurations params
-import awsconfig from './aws-exports'
-
-Amplify.configure(awsconfig)
-//====== End Auth config =====
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <CreatePost />
       <DisplayPosts />
-     
+
+
     </div>
   );
 }
 
-export default withAuthenticator(App, {
-   includeGreetings: true
-});
+
+export default withAuthenticator(App, 
+  { includeGreetings: true});
