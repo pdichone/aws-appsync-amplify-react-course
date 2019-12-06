069abd2e0a9f057f8189e30b9733abfa5f20f06a
Paulo <pdichone@gmail.com>
2019-12-06 12:14:10 -0800

    Updated...
    Updated the entire file.

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -4,27 +4,231 @@
 export const onCreatePost = `subscription OnCreatePost {
   onCreatePost {
     id
-    title
-    body
+    postOwnerId
+    postOwnerUsername
+    postTitle
+    postBody
     createdAt
+    comments {
+      items {
+        id
+        commentOwnerId
+        commentOwnerUsername
+        content
+        createdAt
+      }
+      nextToken
+    }
+    likes {
+      items {
+        id
+        numberLikes
+        likeOwnerId
+        likeOwnerUsername
+      }
+      nextToken
+    }
   }
 }
 `;
 export const onUpdatePost = `subscription OnUpdatePost {
   onUpdatePost {
     id
-    title
-    body
+    postOwnerId
+    postOwnerUsername
+    postTitle
+    postBody
     createdAt
+    comments {
+      items {
+        id
+        commentOwnerId
+        commentOwnerUsername
+        content
+        createdAt
+      }
+      nextToken
+    }
+    likes {
+      items {
+        id
+        numberLikes
+        likeOwnerId
+        likeOwnerUsername
+      }
+      nextToken
+    }
   }
 }
 `;
 export const onDeletePost = `subscription OnDeletePost {
   onDeletePost {
     id
-    title
-    body
+    postOwnerId
+    postOwnerUsername
+    postTitle
+    postBody
     createdAt
+    comments {
+      items {
+        id
+        commentOwnerId
+        commentOwnerUsername
+        content
+        createdAt
+      }
+      nextToken
+    }
+    likes {
+      items {
+        id
+        numberLikes
+        likeOwnerId
+        likeOwnerUsername
+      }
+      nextToken
+    }
+  }
+}
+`;
+export const onCreateComment = `subscription OnCreateComment {
+  onCreateComment {
+    id
+    commentOwnerId
+    commentOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+    content
+    createdAt
+  }
+}
+`;
+export const onUpdateComment = `subscription OnUpdateComment {
+  onUpdateComment {
+    id
+    commentOwnerId
+    commentOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+    content
+    createdAt
+  }
+}
+`;
+export const onDeleteComment = `subscription OnDeleteComment {
+  onDeleteComment {
+    id
+    commentOwnerId
+    commentOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+    content
+    createdAt
+  }
+}
+`;
+export const onCreateLike = `subscription OnCreateLike {
+  onCreateLike {
+    id
+    numberLikes
+    likeOwnerId
+    likeOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const onUpdateLike = `subscription OnUpdateLike {
+  onUpdateLike {
+    id
+    numberLikes
+    likeOwnerId
+    likeOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
+  }
+}
+`;
+export const onDeleteLike = `subscription OnDeleteLike {
+  onDeleteLike {
+    id
+    numberLikes
+    likeOwnerId
+    likeOwnerUsername
+    post {
+      id
+      postOwnerId
+      postOwnerUsername
+      postTitle
+      postBody
+      createdAt
+      comments {
+        nextToken
+      }
+      likes {
+        nextToken
+      }
+    }
   }
 }
 `;
