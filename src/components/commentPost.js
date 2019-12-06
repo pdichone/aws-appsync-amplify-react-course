4fdd1cb744121e202d3d9b4849695d5766dff9a1
Paulo <pdichone@gmail.com>
2019-12-06 12:15:41 -0800

    Updated commentPost

--- src/components/commentPost.js
+++ src/components/commentPost.js
@@ -1,22 +1,26 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class CommentPost extends Component {
 
-  render() {
-    const {content, commentOwnerUsername, createdAt} = this.props.commentData;
-    return (
-      <div className="comment">
-        <span style={{ fontStyle: "italic", color: "#0ca5e297" }}>
-          {"Wrote by: "} {commentOwnerUsername}
-          {" on "}
-          <time style={{ fontStyle: "italic" }} dateTime={createdAt}>
-            {" "}
-            {new Date(createdAt).toDateString()}
-          </time>
-        </span>
-        <p >{content}</p>
-      </div>
-    )
-  }
+
+
+    render() {
+        const { content, commentOwnerUsername, createdAt} = this.props.commentData
+         return (
+                <div className="comment">
+                    <span style={{ fontStyle: "italic", color: "#0ca5e297"}}>
+                        {"Commment by: " } { commentOwnerUsername}
+                        {" on "}
+                        <time style={{ fontStyle: "italic"}}>
+                            { " "}
+                            { new Date(createdAt).toDateString()}
+
+                        </time>
+                    </span>
+                    <p> { content }</p>
+
+                </div>
+         )
+    }
 }
-export default CommentPost;
+export default CommentPost
