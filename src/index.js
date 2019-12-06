b8210586ba51ed7761a83fa8fea586b11c53789e
Paulo <pdichone@gmail.com>
2019-12-06 12:06:07 -0800

    Update index.js
    Removed irrelevant code.

--- src/index.js
+++ src/index.js
@@ -2,41 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import gql from 'graphql-tag';
-import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync'
-import aws_config from './aws-exports'
 import * as serviceWorker from './serviceWorker';
 
+import Amplify from 'aws-amplify'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
 
 
-import { listPosts } from './graphql/queries'
-import { ApolloProvider } from 'react-apollo';
 
-const client = new AWSAppSyncClient({
-     url: aws_config.aws_appsync_graphqlEndpoint,
-     region: aws_config.aws_appsync_region,
-     auth: {
-         type: AUTH_TYPE.API_KEY,
-         apiKey: aws_config.aws_appsync_apiKey
-     }
-})
 
-client.query({
-    query: gql(listPosts)
-}).then(({ data }) => {
-     console.log("Blog data: ", data)
-})
-ReactDOM.render(
-    /*
-    Next we import an ApolloProvider component from the ‘react-apollo’ 
-    and wrap it in our App component 
-    by passing a client so that we can access 
-    the client anywhere from our react app.
-     */
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>, 
-    document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
